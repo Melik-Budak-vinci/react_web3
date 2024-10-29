c5f83fb82b6ebde9a299798071aff8895a750093
melik <melik.budak@student.vinci.be>
2024-10-30 00:46:10 +0100

    module3:loading

--- exercices/modules3/3.1/src/Components/App/App.jsx
+++ exercices/modules3/3.1/src/Components/App/App.jsx
@@ -1,28 +1,37 @@
-import { useState } from 'react'
-import Statistics from '../Statistics/Statistics'
+import { useEffect, useState } from 'react'
+import Statistics from 'components/Statistics/Statistics'
+import Loading from 'components/Loader/Loading'
 
 const App = () => {
   // save clicks of each button to its own state
   const [feedbacks, setFeedbacks] = useState({good:0,neutral:0,bad:0})
+  const [loading, setLoading] = useState(true)
   
 
   const handleGood = () => {
+    setLoading(true)
     const newFeedbacks = {...feedbacks,good:feedbacks.good + 1}
     setFeedbacks(newFeedbacks)
-
       
   }
+  
   const handleNeutral = () => {
+    setLoading(true)
     const newFeedbacks = {...feedbacks,neutral:feedbacks.neutral + 1}
     setFeedbacks(newFeedbacks)
 
 
     }
     const handleBad = () => {
+        setLoading(true)
         const newFeedbacks = {...feedbacks,bad:feedbacks.bad + 1}
         setFeedbacks(newFeedbacks)
     }
-
+    const handleLoading = () => {
+        setLoading(false)
+    
+      }
+    setTimeout(handleLoading, 3000)
   return (
     <div style={{justifyContent:'center',alignItems:'center'}}>
         <h3>give feedback</h3>
@@ -30,8 +39,7 @@ const App = () => {
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleGood}>good</button>
         <h3>statistics</h3>
-        
-        <Statistics feedbacks={feedbacks} />
+        {loading? <Loading/>:<Statistics feedbacks={feedbacks} />}
 
     </div>
   )
